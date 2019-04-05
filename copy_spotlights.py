19bbd716c4ecc380b86058509b0fb21e73b48e08
DevilLordHarsh <devillordharsh@gmail.com>
2019-04-04 21:18:20 -0700

    version2. uses argparse. other improvements made.

--- copy_spotlights.py
+++ copy_spotlights.py
@@ -1,69 +1,127 @@
-from os import path, makedirs, listdir, environ
+from os import path, makedirs, listdir
+# needed to import Path from pathlib because 'HOME' environment variable using os.path returned None on cmd
+from pathlib import Path
 from shutil import copy
 from PIL import Image
-from sys import argv, exit
+from argparse import ArgumentParser
 
 
-def copy_spotlights_images(spotlights_dir, save_dir, split=True, filter_res=1920, img_ext=".png", dir_land="landscape", dir_port="portrait"):
+def find_images(dir):
+  """Given a directory, finds out which files in the root of the directory are images
+
+  Arguments:
+    dir {str} -- directory location to scan for image files
+
+  Raises:
+    AssertionError -- if given directory is not valid
+
+  Returns:
+    list -- list of filenames found in the given directory which are images
+  """
+  assert path.isdir(dir)==True
   
-  """
-  For Windows 10 users, copies beautiful images stored on their machine, called 'Windows Spotlights' to a directory of their choice.
+  im_files = []
+  
+  for item in listdir(dir):
+    item_path = path.normpath(path.join(dir, item))
 
-  Parameters:
-    spotlights_dir (str): directory location where windows_spotlights are stored
-    save_dir (str): directory location where the user wants to save spotlights images
-    split (boolean): saves images inside save_dir in landscape and portrait folders. Default True
-    filter_res (int): defautl resolution used to determine minimum width of an image to decide
-      if it's a portrait image or landscape image. Ignores images with resolution less than defined
-      resolution. Default 1920
-    img_ext (str): the extension to use when saving images. Default ".png"
-    dir_land (str): landscape directory name. Default "landscape"
-    dir_port (str): portrait directory name. Default "portrait"
+    if path.isfile(item_path):
+      try:
+        Image.open(item_path)
+        im_files.append(item)
+      except IOError:
+        pass
     
-  """
+  return im_files
 
+
+def copy_spotlights_images(spotlights_dir, save_dir, split=True, min_res=1920, dir_land="landscape", dir_port="portrait", dir_other="other"):
+  """For Windows 10 users, copies beautiful images stored on their machine, called 'Windows Spotlights' to a directory of their choice.
+  
+  Arguments:
+    spotlights_dir {str} -- directory location where windows 10 stores spotlight images
+    save_dir {str} -- directory location where spotlight images will be copied to
+  
+  Keyword Arguments:
+    split {bool} -- split images into different folders based on types such as landscape and portrait (default: {True})
+    min_res {int} -- ignore images with horizontal or vertical resolution lower than min_res value,  (default: {1920})
+    dir_land {str} -- directory name in which to store landscape images (default: {"landscape"})
+    dir_port {str} -- directory name in which to store portrait images (default: {"portrait"})
+    dir_other {str} -- directory name in which to store images other than portrait or landscape type (default: {"other"})
+  
+  Raises:
+    Exception -- raises exception if save_dir is not valid
+  """
   if not path.isdir(save_dir):
     raise Exception(f"'{save_dir}' does not seem to be a valid directory!")
 
-  complete_landscape_path, complete_portrait_path = save_dir, save_dir
+  complete_landscape_path, complete_portrait_path, complete_other_path = "", "", ""
 
   if split:
-    # complete paths to landscape and portrait directories inside the save location
-    complete_landscape_path = path.join(save_dir, dir_land)
-    complete_portrait_path = path.join(save_dir, dir_port)
+    # get complete paths to landscape, portrait, and other directories inside the save directory
+    complete_landscape_path = path.normpath(path.join(save_dir, dir_land))
+    complete_portrait_path = path.normpath(path.join(save_dir, dir_port))
+    complete_other_path = path.normpath(path.join(save_dir, dir_other))
 
-    # make directories if they don't exist already
+    # make required directories if they don't exist
     if not path.isdir(complete_landscape_path):
       makedirs(complete_landscape_path)
     if not path.isdir(complete_portrait_path):
       makedirs(complete_portrait_path) 
+    if not path.isdir(complete_other_path):
+      makedirs(complete_other_path) 
 
-  for spotlight in listdir(spotlights_dir):
-    sl = path.join(spotlights_dir, spotlight)
+  else:
+    # don't copy spotlight images into different folders
+    complete_landscape_path, complete_portrait_path, complete_other_path = save_dir, save_dir, save_dir
+
+  for image_file in find_images(spotlights_dir):
+    image_file_full_path = path.normpath(path.join(spotlights_dir, image_file))
+
+    im = Image.open(image_file_full_path)
+
+    # save image with an extension as read by PIL.Image module
+    save_image_file_name = image_file + "." + im.format
+    where = None
     
-    im = Image.open(sl)
+    im_width, im_height = int(im.size[0]), int(im.size[1])
 
-    if (int(im.size[0]) >= filter_res):
-      copy(sl, path.join(complete_landscape_path, spotlight+img_ext))
-    elif (int(im.size[1]) >= filter_res):
-      copy(sl, path.join(complete_portrait_path, spotlight+img_ext))
+    if im_width >= min_res or im_height >= min_res:
+      if (im_height < im_width):
+        where = path.normpath(path.join(complete_landscape_path, save_image_file_name))
 
+      elif (im_width < im_height):
+        where = path.normpath(path.join(complete_portrait_path, save_image_file_name))
+        
+      else:
+        where = path.normpath(path.join(complete_other_path, save_image_file_name))
+    
+    if where != None:
+      copy(image_file_full_path, where)
+    
 
 if __name__ == "__main__":
 
+  # Argument Parser to enable use of this program using bash terminal
+  parser = ArgumentParser()
+  parser.add_argument("save_dir", type=str, help="directory location where spotlight images will be copied to")
+  parser.add_argument("--no-split", dest="no_split", action="store_true", help="portrait and landscape images will not be placed into separate folders")
+  parser.add_argument("--min-res", dest="min_res", type=int, help="ignore images with horizontal or vertical resolution lower than min_res value, default:1920", default=1920)
+  parser.add_argument("--dir-land", dest="dir_land", type=str, help="directory name in which to store landscape images, default:landscape", default="landscape")
+  parser.add_argument("--dir-port", dest="dir_port", type=str, help="directory name in which to store portrait images, default:portrait", default="portrait")
+  parser.add_argument("--dir-other", dest="dir_other", type=str, help="directory name in which to store images of equal resolution, default:other", default="other")
+  args = parser.parse_args()
+
   # Known directory location path where windows stores spotlight images
-  HOME = environ.get("HOME")
-  SPOTLIGHT_DIR = path.join(HOME, 'AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets')
+  home = str(Path.home())
+  spotlight_dir = path.join(home, 'AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets')
 
-  if len(argv) < 2:
-    exit("No argument for a save directory was received. Exiting!")
+  save_dir = args.__getattribute__("save_dir")
+  split = not args.__getattribute__("no_split")
+  min_res = args.__getattribute__("min_res")
+  dir_land = args.__getattribute__("dir_land")
+  dir_port = args.__getattribute__("dir_port")
+  dir_other = args.__getattribute__("dir_other")
 
-  save_dir = argv[1]
-  split = True
-  
-  if len(argv) == 3:
-    split = False
-
-  copy_spotlights_images(SPOTLIGHT_DIR, save_dir, split)
-  
+  copy_spotlights_images(spotlight_dir, save_dir, split, min_res, dir_land, dir_port, dir_other)
   
