9f19a66559619bd1a6eff0c446019a178c3b73f8
Harsh <39079018+DevilLordHarsh@users.noreply.github.com>
2019-04-06 16:44:26 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ unzip $TEMP/copy-windows10-spotlight-images-master.zip -d $HOME
 rm $TEMP/copy-windows10-spotlight-images-master.zip
 
 cd $HOME/copy-windows10-spotlight-images-master
+pip install pipenv
 pipenv install
 echo 'alias get-spotlights="cd $HOME/copy-windows10-spotlight-images-master/; pipenv run python copy_spotlights.py"' >> $HOME/.bashrc
