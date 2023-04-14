7e084a40b33f9824ef637e31dd634118ae51f029
Harsh <ksharshveer@gmail.com>
2023-04-14 12:06:10 -0700

    Minor version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='get-spotlights',
-    version='3.1',
+    version='3.2',
     install_requires='pillow',
     packages=['copy_spotlights'],
     entry_points = {
